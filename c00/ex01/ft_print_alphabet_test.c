5193dd8de0885a07cfc42aa12085a3493f9fd638
Yew chiang Tang <ytang@u91z05s03.42kl.edu.my>
2021-06-28 15:15:44 +0800

    add ex01

---
+++ c00/ex01/ft_print_alphabet_test.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	Alpha;
+
+	Alpha = 'a';
+	while (Alpha <= 'z')
+	{
+		ft_putchar(Alpha);
+		Alpha++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}

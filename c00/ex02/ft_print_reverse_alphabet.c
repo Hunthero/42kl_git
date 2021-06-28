ec3c2c7520318e569bd2cfa9b13f34fbede62f83
Yew chiang Tang <ytang@u91z05s03.42kl.edu.my>
2021-06-28 15:36:49 +0800

    addex02

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void ft_print_reverse_alphabet()
+{
+	char Alpha;
+	Alpha = 'z';
+	while (Alpha >= 'a')
+	{
+		ft_putchar(Alpha);
+		Alpha--;
+	}
+}
+
+int main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
